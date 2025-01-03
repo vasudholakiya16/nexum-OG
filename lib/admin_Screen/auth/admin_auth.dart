1c0a9c80d748dec0c7fc3d710cd5aa69de076c92
vasudholakiya16 <122475789+vasudholakiya16@users.noreply.github.com>
2025-01-03 09:22:31 +0530

    feat: implement Firebase authentication and user management, update app branding and icons

--- lib/admin_Screen/auth/admin_auth.dart
+++ lib/admin_Screen/auth/admin_auth.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/button.dart';
 
 class AdminAuth extends StatefulWidget {
   const AdminAuth({super.key});
@@ -8,8 +9,104 @@ class AdminAuth extends StatefulWidget {
 }
 
 class _AdminAuthState extends State<AdminAuth> {
+  bool _isPasswordVisible =
+      false; // State variable to toggle password visibility
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
+
+    return Scaffold(
+      backgroundColor: const Color(0xffFFFCEF),
+      body: Padding(
+        padding: EdgeInsets.only(top: mediaQuery.size.height * 0.2),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/thumbnil.png',
+              // width: screenWidth * 0.8,
+            ),
+            const Center(
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: screenWidth * 0.9,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: const Border(
+                    top: BorderSide(color: Colors.black, width: 1),
+                    left: BorderSide(color: Colors.black, width: 1),
+                    right: BorderSide(color: Colors.black, width: 5),
+                    bottom: BorderSide(color: Colors.black, width: 4),
+                  ),
+                ),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.email),
+                    contentPadding: EdgeInsets.symmetric(vertical: 20),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: screenWidth * 0.9,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: const Border(
+                    top: BorderSide(color: Colors.black, width: 1),
+                    left: BorderSide(color: Colors.black, width: 1),
+                    right: BorderSide(color: Colors.black, width: 5),
+                    bottom: BorderSide(color: Colors.black, width: 4),
+                  ),
+                ),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText:
+                      !_isPasswordVisible, // Toggle password visibility
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: InputBorder.none,
+                    prefixIcon: const Icon(Icons.lock),
+                    contentPadding: const EdgeInsets.symmetric(vertical: 20),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: RoundButton(title: "Login", onTap: () {}),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/common/customText.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+Widget customTextField({
+  required String hint,
+  required String title,
+  required TextEditingController controller,
+  required bool isPass,
+  required RegExp validationRegulatorExpression,
+  required FormFieldValidator<String> validator,
+  required VoidCallback toggleVisibility,
+  required bool isPasswordVisible,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      // if (title != null) title.text.color(Colors.black).fontFamily(semibold).size(16).make(),
+//       5.heightBox,
+      Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: TextFormField(
+          controller: controller,
+          obscureText: isPass && !isPasswordVisible,
+          decoration: InputDecoration(
+            labelText: title,
+            hintText: hint,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(40.0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Colors.black,
+              ),
+              borderRadius: BorderRadius.circular(40.0),
+            ),
+            suffixIcon: isPass
+                ? IconButton(
+                    icon: Icon(
+                      isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Colors.black,
+                    ),
+                    onPressed: toggleVisibility,
+                  )
+                : null,
+          ),
+          validator: validator,
+        ),
+      ),
+    ],
+  );
+}
